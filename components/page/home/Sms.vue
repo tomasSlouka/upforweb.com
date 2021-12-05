0628ed5e953bc52a68961b0f4c3f2eae33a509c2
tomasSlouka <tomas.slouka@gmail.com>
2021-12-06 00:48:17 +0100

    Update SMS

--- components/page/home/Sms.vue
+++ components/page/home/Sms.vue
@@ -9,15 +9,15 @@
         <div class='mt20 grid gap-20'>
             <div class="message justify-self-end">
                 <div class='grid gap-2 col-2 auto justify-content-end time'>
-                    <div>6.11. 9:14</div>
+                    <div>5.12. 9:14</div>
                     <div><img src="@/assets/img/sim_card_black_24dp.svg" alt="" class='grayscale' /></div>
                 </div>
                 <div class="body">
-                    NCZI: ANNA SLOUKOVA Ing, 1965, COVID-19 PASS: D41-290-122, Typ: PCR, odber zo dna 7.11.2021 je NEGATIVNY. V pripade zmeny zdravotneho stavu volajte vseobecneho lekara.
+                    NCZI: ANNA SLOUKOVA Ing, 1965, COVID-19 PASS: D41-290-122, Typ: PCR, odber zo dna 4.12.2021 je NEGATIVNY. V pripade zmeny zdravotneho stavu volajte vseobecneho lekara.
                 </div>
             </div>
 
-            <div class="message justify-self-start">
+            <!-- <div class="message justify-self-start">
                 <div class='grid gap-2 col-2 auto justify-content-start time'>
                     <div><img src="@/assets/img/sim_card_black_24dp.svg" alt="" class='grayscale' /></div>
                     <div>6.11. 9:14</div>
@@ -25,11 +25,15 @@
                 <div class="body">
                     NCZI: ANNA SLOUKOVA Ing, 1965, COVID-19 PASS: D41-290-122, Typ: PCR, odber zo dna 7.11.2021 je NEGATIVNY. V pripade zmeny zdravotneho stavu volajte vseobecneho lekara.
                 </div>
-            </div>
+            </div> -->
         </div>
 
-        <div class='absolute'>
-
+        <div class='write absolute p20'>
+            <div class="grid gap-10">
+                <div class="plus grid align-items-center justify-items-center justify-self-start align-self-end"><img src="@/assets/img/add_black_24dp.svg" alt="" /></div>
+                <div contenteditable="true" class='input justify-self-stretch'>Textová správa</div>
+                <div class="send grid align-items-center justify-items-center justify-self-end align-self-end"><img src="@/assets/img/arrow_upward_black_24dp.svg" alt="" /></div>
+            </div>
         </div>
 
     </div>
@@ -54,6 +58,7 @@ export default {
     font-family: 'Roboto', sans-serif;
     font-size: 16px;
     padding: 21px;
+    min-height: 100vh;
 }
 .name {
     font-weight: 600;
@@ -72,7 +77,42 @@ export default {
 
 .message .body {
     padding: 20px;
-    border-radius: 10px;
+    border-radius: 20px;
     background-color: #F6F6F6;
+}
+
+.write {
+    bottom: 0px;
+    left: 0px;
+    min-width: 100vw;
+}
+.write > div {
+    grid-template-columns: 40px 1fr 40px
+}
+.plus {
+    background-color: #F3F3F3;
+    /* max-width: 75px; */
+    /* max-height: 75px; */
+    border-radius: 75px;
+    padding: 10px;
+}
+.plus img {
+    height: 18px;
+}
+
+.input {
+    background-color: #F1F1F1;
+    padding: 21px;
+    border-radius: 20px;
+}
+.send {
+    background-color: #F3F3F3;
+    /* max-width: 75px; */
+    /* max-height: 75px; */
+    border-radius: 75px;
+    padding: 10px;
+}
+.send img {
+    height: 18px;
 }
 </style>
