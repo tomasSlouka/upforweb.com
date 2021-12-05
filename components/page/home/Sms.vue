1b1b5978bd7782a70a03369237d3076c1c5d1277
tomasSlouka <tomas.slouka@gmail.com>
2021-12-05 23:40:04 +0100

    Update SMS

---
+++ components/page/home/Sms.vue
@@ -0,0 +1,78 @@
+<template>
+    <div class="component relative" id="sms">
+
+        <div class="header grid relative align-items-center">
+            <img src="@/assets/img/arrow_back_black_24dp.svg" alt="" class='absolute' />
+            <div class="name justify-self-center grid align-items-center">NCZI</div>
+        </div>
+
+        <div class='mt20 grid gap-20'>
+            <div class="message justify-self-end">
+                <div class='grid gap-2 col-2 auto justify-content-end time'>
+                    <div>6.11. 9:14</div>
+                    <div><img src="@/assets/img/sim_card_black_24dp.svg" alt="" class='grayscale' /></div>
+                </div>
+                <div class="body">
+                    NCZI: ANNA SLOUKOVA Ing, 1965, COVID-19 PASS: D41-290-122, Typ: PCR, odber zo dna 7.11.2021 je NEGATIVNY. V pripade zmeny zdravotneho stavu volajte vseobecneho lekara.
+                </div>
+            </div>
+
+            <div class="message justify-self-start">
+                <div class='grid gap-2 col-2 auto justify-content-start time'>
+                    <div><img src="@/assets/img/sim_card_black_24dp.svg" alt="" class='grayscale' /></div>
+                    <div>6.11. 9:14</div>
+                </div>
+                <div class="body">
+                    NCZI: ANNA SLOUKOVA Ing, 1965, COVID-19 PASS: D41-290-122, Typ: PCR, odber zo dna 7.11.2021 je NEGATIVNY. V pripade zmeny zdravotneho stavu volajte vseobecneho lekara.
+                </div>
+            </div>
+        </div>
+
+        <div class='absolute'>
+
+        </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+
+    // props: ['domainList'],
+    data() {
+        return {
+        }
+    },
+    methods: {
+    }
+}
+</script>
+
+<style scoped>
+
+#sms {
+    font-family: 'Roboto', sans-serif;
+    font-size: 16px;
+    padding: 21px;
+}
+.name {
+    font-weight: 600;
+    font-size: 18px;
+}
+.message {
+    max-width: 90%;
+}
+.message .time {
+    font-size: 14px;
+    color: #797979;
+}
+.message img {
+    height: 12px;
+}
+
+.message .body {
+    padding: 20px;
+    border-radius: 10px;
+    background-color: #F6F6F6;
+}
+</style>
---
+++ pages/sms.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class='page'>
+        <Sms />
+    </div>
+</template>
+
+<script>
+import Sms from '@/components/page/home/Sms.vue'
+
+export default {
+    layout: 'defaultPage',
+        components: {
+            Sms
+    },
+}
+</script>
+
+<style>
+
+</style>
